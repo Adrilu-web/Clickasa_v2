eb050c9b5a1af75c3bd494643936562e9ff586ff
Adri <adrimeluz@gmail.com>
2024-12-22 13:17:43 -0300

    Mas cambios

--- js/script.js
+++ js/script.js
@@ -14,9 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
             data.forEach((inmueble, indice) => {
                 card.innerHTML += `
                 <div class="propiedad_card">
-                    <img src="${inmueble.img}" alt="${inmueble.altimg}">
-                    <h4>${inmueble.titulo}</h4>
-                    <p>Precio: u$s${inmueble.precio} -   
+                    <img src="${inmueble.img}" alt=${inmueble.direccion}" width=300 height="200">
+                    <h4>${inmueble.operacion} - ${inmueble.tipo_inmueble}</h4>
+                    <p>${inmueble.direccion}</p> 
+                    <p>Precio: ${inmueble.precio.toLocaleString("en-CA", {style:"currency", currency:"USD"})}<br>   
                     ${inmueble.descripcion_corta}</p>           
                     <button class="vermas_btn" data-index="${indice}">Ver Más</button>
                     <button class="favoritos_btn" data-index="${indice}">Agregar a Favoritos</button>
@@ -48,9 +49,6 @@ document.addEventListener("DOMContentLoaded", () => {
         
         .catch(error => console.error('Error al cargar el archivo JSON:', error));
 
-
-
-
     // Función para agregar a favoritos
     function agregarFav(inmueble) {
         let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []; 
@@ -58,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
         //LocalStorage
         if (typeof(Storage) !== "undefined") {
             //ver si es un inmueble repetido
-            if (!favoritos.some(fav => fav.titulo === inmueble.titulo)) {
+            if (!favoritos.some(fav => fav.id === inmueble.id)) {
                 favoritos.push(inmueble);
                 localStorage.setItem('favoritos', JSON.stringify(favoritos));
                 alert('Inmueble agregado a favoritos.');
@@ -74,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function mostrarFav() {
         let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []; 
         const container = document.getElementById('mostrarFav');
-        container.innerHTML = '<h4>Favoritos</h4>'
+        container.innerHTML = '<h4>Lista de Inmuebles Favoritos</h4>'
           if (favoritos.length === 0) {
             container.innerHTML = '<h4>No hay inmuebles en favoritos.</h4>';
         } 
@@ -86,8 +84,9 @@ document.addEventListener("DOMContentLoaded", () => {
             favoritos.forEach((inmueble, index) => {
                 card.innerHTML += `
                 <div class="favoritos_card">
-                        <h4>${inmueble.titulo}</h4>
-                        <p>Precio: u$s${inmueble.precio}</p>  
+                        <h4>${inmueble.operacion} - ${inmueble.tipo_inmueble}</h4>
+                        <p>${inmueble.direccion}</p>
+                        <p>Precio: ${inmueble.precio.toLocaleString("en-CA", {style:"currency", currency:"USD"})}</p>  
                         <button class="elimfav_btn" data-index="${index}">Eliminar</button>
                 </div>       
                 `; 
@@ -111,7 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const elemento = document.getElementsByClassName('favoritos_card');
 
             elemento[id].remove();       
-            //ojo dar de baja 1 dar de alta 2 y borrar 1
+
         }
     }
 
@@ -127,26 +126,26 @@ function verDetalles(inmueble) {
                 <span class="close_btn">&times;</span>
   				<div class="carousel_content">
                     <div class="carousel_elemento">
-                        <img src="${inmueble.img}" alt="${inmueble.altimg}" width=200 height="200">
+                        <img src="${inmueble.img}" alt="${inmueble.direccion}" width=200 height="200">
                     </div>
                     <div class="carousel_elemento">
-                        <img src="${inmueble.img1}" alt="" width=200 height="200">
+                        <img src="${inmueble.img1}" alt="${inmueble.direccion}" width=200 height="200">
                     </div>
                     <div class="carousel_elemento">
-                        <img src="${inmueble.img2}" alt="" width=200 height="200">
+                        <img src="${inmueble.img2}" alt="${inmueble.direccion}" width=200 height="200">
                     </div>
                     <div class="carousel_elemento">
-                        <img src="${inmueble.img3}" alt="" width=200 height="200">
+                        <img src="${inmueble.img3}" alt="${inmueble.direccion}" width=200 height="200">
                     </div>
-                                        <div class="carousel_elemento">
-                        <img src="${inmueble.img4}" alt="" width=200 height="200">
+                    <div class="carousel_elemento">
+                        <img src="${inmueble.img4}" alt="${inmueble.direccion}" width=200 height="200">
                     </div>
-                                        <div class="carousel_elemento">
-                        <img src="${inmueble.img5}" alt="" width=200 height="200">
+                    <div class="carousel_elemento">
+                        <img src="${inmueble.img5}" alt="${inmueble.direccion}" width=200 height="200">
                     </div>
                 </div>
-                <h4>${inmueble.titulo}</h4>
-                <p>Precio: u$s${inmueble.precio}</p>
+                <h4>${inmueble.operacion}-${inmueble.tipo_inmueble}-${inmueble.direccion}</h4>
+                <p><strong>Precio: ${inmueble.precio.toLocaleString("en-CA", {style:"currency", currency:"USD"})}</strong></p>
                 <p>${inmueble.descripcion_larga}</p>     
             </div>
         `;
